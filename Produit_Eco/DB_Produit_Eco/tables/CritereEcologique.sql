3d2142cdbca2850a88c01cb22d9ebdb8d3b94fb0
Hanane Chyabri <chyabri.hanane@gmail.com>
2024-02-20 09:35:51 +0100

    first commit

---
+++ Produit_Eco/DB_Produit_Eco/tables/CritereEcologique.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[CritereEcologique]
+(
+	[Id_CritereEco] INT NOT NULL PRIMARY KEY,
+	[Nom_CritereEco] NVARCHAR(100) NOT NULL
+)
---
+++ Produit_Eco/DB_Produit_Eco/tables/Produit.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Produit]
+(
+    [Id_Produit] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    [Nom] NVARCHAR(100) NOT NULL,
+    [Description] NVARCHAR(255) NOT NULL,
+    [url_image] NVARCHAR(512) NOT NULL,
+    [prix] DECIMAL(10, 2) NOT NULL,
+    [Nombre_vente] INT NULL
+)
